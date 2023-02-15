87dc765ba076f0583928620b92e2ade7bf5c861b
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-16 02:04:36 +0530

    add a javascript dom card create.

---
+++ main.js
@@ -0,0 +1,245 @@
+let footCard = [{
+    id : 1 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bwkfac2r3bpphgfm7rh9",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 2 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/cyxyhx6wzgi9ogyapxym",
+    name : "Burger King",
+    cusine : "Burgers, American",
+    rating : 4.1,
+    prepareFood : "53 mins",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 3 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/o5vlkrbbjez8gbei4utj",
+    name : "Kakada Ramprasad",
+    cusine : "North Indian, Tandoor",
+    rating : 4.5 ,
+    prepareFood : "33 mins",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 4 ,
+    image :"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/zggqzcw1muhht6tkwuba", 
+    name : "Moonlight Takeaway & Delivery",
+    cusine : "Indian, Chinese",
+    rating : 4.0 ,
+    prepareFood : "43 mins",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 5 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ac1staxtetolkvynv2ot",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 6 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/gbuvu9fca82lu0r7mdxl",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 7 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 8 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hdatwmjnajz1htglkw7o",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 9 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/biztrxunzi2lklnrzgjf",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 10 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/oodghmivnc6bxnbrvzzw",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 11 ,
+    image : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/p6w2bhhag5nym91k1wn8",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 12 ,
+    image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/jk1nao7hohp8lreel9vh",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 13 ,
+    image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wushsl0ncurfnblysdbb",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 14 ,
+    image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/eyfung1qwm35weq4uapt",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 15 ,
+    image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/cmwqxtqo6zjkdfjxcjr0",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+},
+{
+    id : 16 ,
+    image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/l96wmarvcufgmgfhotoi",
+    name : "Shree Mithai",
+    cusine : "Desserts, Street Food, Chaat",
+    rating : 4.4 ,
+    prepareFood : "31 MINs",
+    foodPrice : "$ 250 for two",
+    offer : "50% off | Use WELCOME50",
+    hover : " QUICK VIEW"
+}   
+]
+
+let cont = document.querySelector('#container');
+let row = document.createElement("div");
+row.classList.add("row-card");
+cont.appendChild(row);
+
+footCard.forEach(function(e){
+    let colum = document.createElement("div");
+    colum.classList.add("col");
+    row.appendChild(colum);
+
+    let colCard = document.createElement("div");
+    colCard.classList.add("col-card");
+    colum.appendChild(colCard);
+
+    let imageURL = document.createElement("div");
+    imageURL.classList.add("img-div");
+    colCard.appendChild(imageURL);
+
+    let imgurl = document.createElement("img");
+    imgurl.src=e.image;
+    imageURL.appendChild(imgurl);
+
+    let content = document.createElement("div");
+    content.classList.add("content-head");
+    colCard.appendChild(content);
+
+    let head = document.createElement("h2");
+    head.innerHTML=e.name;
+    content.appendChild(head);
+
+    let para = document.createElement("p");
+    para.innerHTML=e.cusine;
+    content.appendChild(para);
+
+    let content2 = document.createElement("div");
+    content2.classList.add("content-rate");
+    colCard.appendChild(content2);
+
+    let rate = document.createElement("span");
+    rate.innerHTML=e.rating;
+    content2.appendChild(rate);
+
+    let  preFood = document.createElement("p");
+    preFood.innerHTML=e.prepareFood;
+    content2.appendChild(preFood);
+
+    let foodP = document.createElement("p");
+    foodP.innerHTML=e.foodPrice;
+    content2.appendChild(foodP);
+
+    let off = document.createElement("div");
+    off.classList.add("off-con");
+    colCard.appendChild(off);
+
+    let offoff = document.createElement("p");
+    offoff.innerHTML=e.offer;
+    off.appendChild(offoff);
+
+    let hr = document.createElement("hr");
+    off.appendChild(hr)
+
+    let hov = document.createElement("h3");
+    hov.innerHTML=e.hover;
+    off.appendChild(hov);
+
+})
+
